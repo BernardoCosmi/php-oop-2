407401b9b52177e30a4a37532db9d263d9cfaee8
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-03-10 18:59:46 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once __DIR__ . '/models/Product.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-2</title>
+</head>
+<body>
+    
+</body>
+</html>
